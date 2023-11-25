456147b1ac5844dc3a1cb69963c60f103a2e9df9
Dani <enderfinofino666@gmail.com>
2023-11-25 01:56:19 -0500

    first commit

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from authentication import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view()),
+]
